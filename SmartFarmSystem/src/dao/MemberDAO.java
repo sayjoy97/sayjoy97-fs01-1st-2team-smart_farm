d2e803fd9274ad77c99922ac0033a7c174f1698c
sayjoy97 <sayjjoy97@gmail.com>
2025-10-21 18:02:41 +0900

    Delete SmartFarmSystem directory

--- SmartFarmSystem/src/dao/MemberDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import dto.MemberDTO;
-
-public interface MemberDAO {
-	int register(MemberDTO user);
-	MemberDTO login(String id, String pass);
-}
--- SmartFarmSystem/src/service/SensorDataService.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public interface SensorDataService {
-	 void saveData(String topic, String payload);
-}
