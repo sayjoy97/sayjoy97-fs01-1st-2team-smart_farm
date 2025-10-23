960360b2ba4d518bcb26f371691deb5967d8903e
sayjoy97 <Admin@DESKTOP-BGELFKJ>
2025-10-23 18:33:18 +0900

    [팜 슬롯에 식물 심기 기능 및 데이터베이스에 값 넣는 기능 구현]

---
+++ src/sql/project_smartfarm_insert_plant_presets.sql
@@ -0,0 +1,15 @@
+-- 상추 (온도 18~22℃ / 습도 60~70% / CO₂ 800~1000ppm / 토양수분 60~70%)
+INSERT INTO plant_presets (plant_name, optimal_temp, optimal_humidity, light_intensity, co2_level, soil_moisture, growth_period_days)
+VALUES ('상추', 20, 65, 70, 900, 65, 30);
+
+-- 딸기 (온도 20~25℃ / 습도 60~75% / CO₂ 1000ppm / 토양수분 65~75%)
+INSERT INTO plant_presets (plant_name, optimal_temp, optimal_humidity, light_intensity, co2_level, soil_moisture, growth_period_days)
+VALUES ('딸기', 22.5, 67.5, 70, 1000, 70, 90);
+
+-- 바질 (온도 23~28℃ / 습도 55~65% / CO₂ 700~900ppm / 토양수분 60%)
+INSERT INTO plant_presets (plant_name, optimal_temp, optimal_humidity, light_intensity, co2_level, soil_moisture, growth_period_days)
+VALUES ('바질', 25.5, 60, 65, 800, 60, 40);
+
+-- 와사비 (온도 10~18℃ / 습도 80~90% / CO₂ 700~900ppm / 토양수분 85~95%)
+INSERT INTO plant_presets (plant_name, optimal_temp, optimal_humidity, light_intensity, co2_level, soil_moisture, growth_period_days)
+VALUES ('와사비', 14, 85, 60, 800, 90, 180);
